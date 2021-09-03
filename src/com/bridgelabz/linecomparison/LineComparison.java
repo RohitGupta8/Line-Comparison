d63545f1af9642c0ee4ea518f7342c6178faf00e
RohitGupta8 <therohitg@gmail.com>
2021-09-03 11:40:51 +0530

    calculate line length by cartesian system

---
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.linecomparison;
+
+import java.util.Scanner;
+
+public class LineComparison {
+    public int comparison () {
+        double x1, x2, y1, y2;
+
+        double dis;
+
+        Scanner sc = new Scanner ( System.in );
+
+        System.out.print ( "enter x1 point: " );
+
+        x1 = sc.nextInt ();
+
+        System.out.print ( "enter y1 point: " );
+
+        y1 = sc.nextInt ();
+
+        System.out.print ( "enter x2 point: " );
+
+        x2 = sc.nextInt ();
+
+        System.out.print ( "enter y2 point: " );
+
+        y2 = sc.nextInt ();
+
+        dis = Math.sqrt ( (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) );
+
+        System.out.println ( "distance between" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ")===>" + dis );
+
+        return (int) dis;
+    }
+
+   
+}
