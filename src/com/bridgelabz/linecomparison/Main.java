e7b2219445d25d5aba7ab3803013532794cb4484
RohitGupta8 <therohitg@gmail.com>
2021-09-03 11:36:00 +0530

    Welcome To LIne Comparison Computation

---
+++ src/com/bridgelabz/linecomparison/Main.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.linecomparison;
+
+public class Main {
+
+    public static void main ( String[] args ) {
+//        LineComparison lineComparison = new LineComparison ();
+        System.out.println ( "*********************************************\n    Welcome To Line Comparison Computation \n*********************************************" );
+//        System.out.println ( "\nEnter line 1 coordinates" );
+//        int line1 = lineComparison.comparison ();
+    }
+}
